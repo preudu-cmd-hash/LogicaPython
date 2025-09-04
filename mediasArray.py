7e62bdd95d78634ed088b5ca8618cd23671b167b
Pedro Henrique <pedrohenrique010123456@gmail.com>
2025-09-04 20:48:45 -0300

    Update mediasArray.py

--- mediasArray.py
+++ mediasArray.py
@@ -1,16 +1,20 @@
-def main():
+def main(): # Função principal 
     notas = []
+    
     for i in range(1, 5):
         nota = float(input(f"Digite a nota {i}: "))
         notas.append(nota)
-    mediaNotas = media(notas)
+        
+    mediaNotas = media(notas) # Envia as notas para calcular a média
+    
     print(f"As notas desse aluno são: {notas}, e a média das notas desse aluno é: {mediaNotas}")
-    if mediaNotas >= 6:
+    
+    if mediaNotas >= 6: # Condicional de aprovação
         print("APROVADO!")
     else:
         print("REPROVADO!")
 
-def media(notas):
+def media(notas): # Cálculo da média
     return sum(notas) / len(notas)
 
 main()
