05171b8afda93c38bfba5a54c74a9fbd54870ec5
Pedro Henrique <pedrohenrique010123456@gmail.com>
2025-09-04 20:52:09 -0300

    Update imcGenero.py

--- imcGenero.py
+++ imcGenero.py
@@ -1,15 +1,15 @@
-def inicio():
+def inicio(): # Função principal, onde chama as outras funções
     nome = str(input("Digite seu nome: "))
     genero_informado = str(input("Qual seu gênero? (M ou F)"))
     peso = float(input("Quanto você pesa? (55.5)"))
     altura = float(input("Qual sua altura? (1.55)"))
-    imcValor = imc(peso, altura)
-    print(f"Olá {nome} o seu imc é {imcValor: .2f}! Está classificado como {classificacao_genero(genero_informado, imcValor)}")
+    imcValor = imc(peso, altura) # Envia os parâmetros para o cálculo do IMC
+    print(f"Olá {nome} o seu imc é {imcValor: .2f}! Está classificado como {classificacao_genero(genero_informado, imcValor)}") # Envia os parâmetros para a diferenciação por gênero e imprime o resultado do IMC e da classificação 
 
-def imc(peso:float, altura:float):
+def imc(peso:float, altura:float): # Função que calcula o IMC
     return peso / (altura*altura)
 
-def classificacao_genero(genero:str, imc:float):
+def classificacao_genero(genero:str, imc:float): # Função que faz a diferenciação do IMC por gênero
     match genero.upper():
         case "M":
             if imc < 20:
