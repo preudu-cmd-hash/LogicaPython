208ee9938af1701f637c58512307644dadc0895c
Pedro Henrique <pedrohenrique010123456@gmail.com>
2025-09-04 20:47:02 -0300

    Update potenciaRecursiva.py

--- potenciaRecursiva.py
+++ potenciaRecursiva.py
@@ -1,10 +1,10 @@
-def main():
+def main(): # Função principal do código que chama a função de potência
     base = int(input("Digite a base: "))
     expoente = int(input("Digite o expoente: "))
 
     print(f"O resultado é: {pot(base, expoente)}")
 
-def pot(base:int, expoente:int):
+def pot(base:int, expoente:int): # Função que calcula a potenciação
     if expoente == 0:
         return 1
     else:
