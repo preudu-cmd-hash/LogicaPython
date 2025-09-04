caf5d1ab4b2858d578f545e9f0686d317ad42e1f
Pedro <pedrohenrique010123456@gmail.com>
2025-09-04 19:14:43 -0300

    sintaxe melhorada

--- mediasArray.py
+++ mediasArray.py
@@ -1,11 +1,11 @@
 def main():
     notas = []
-    for i in range(0, 4):
-        nota = float(input(f"Digite a nota {i + 1}: "))
+    for i in range(1, 5):
+        nota = float(input(f"Digite a nota {i}: "))
         notas.append(nota)
     mediaNotas = media(notas)
-    print(f"A média das notas desse aluno é: {mediaNotas}")
-    if mediaNotas > 6:
+    print(f"As notas desse aluno são: {notas}, e a média das notas desse aluno é: {mediaNotas}")
+    if mediaNotas >= 6:
         print("APROVADO!")
     else:
         print("REPROVADO!")
